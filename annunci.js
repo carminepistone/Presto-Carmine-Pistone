83245483f77d69321758649e3f217a82309de8d5
Carmine Pistone <carmine.pistone.apostoli@gmail.com>
2026-02-13 12:20:08 +0100

    filtri collegati termminati

--- annunci.js
+++ annunci.js
@@ -1,46 +1,48 @@
-fetch("./annunci.json").then((response)=> response.json()).then((data)=>{
-
-
-
-    data.sort((a, b) => a.price - b.price);
-
-    let radioWrapper = document.querySelector("#radioWrapper");
-    let cardWrapper = document.querySelector("#cardWrapper");
-
-
+fetch("./annunci.json")
+    .then((response) => response.json())
+    .then((data) => {
+        data.sort((a, b) => a.price - b.price);
+        let radioWrapper = document.querySelector("#radioWrapper");
+        let cardWrapper = document.querySelector("#cardWrapper");
+        
         function radioCreate() {
             let categories = data.map((annuncio) => annuncio.category);
             let uniqueCategories = Array.from(new Set(categories));
             
-            uniqueCategories.forEach((category, id) => {
+
+            let divAll = document.createElement("div");
+            divAll.classList.add("form-check");
+            divAll.innerHTML = `
+                <input class="form-check-input" type="radio" name="categories" id="All" checked>
+                <label class="form-check-label" for="All">Tutti</label>
+            `;
+            radioWrapper.appendChild(divAll);
+            
+            uniqueCategories.forEach((category) => {
                 let div = document.createElement("div");
                 div.classList.add("form-check");
                 div.innerHTML = `
                     <input class="form-check-input" type="radio" name="categories" id="${category}">
                     <label class="form-check-label" for="${category}">${category}</label>
                 `;
-                
                 radioWrapper.appendChild(div);
             });
         }
         
         radioCreate();
-
-
-        function truncateWord(string){
-            if(string.length > 15){
+        
+        function truncateWord(string) {
+            if (string.length > 15) {
                 return string.split(" ")[0] + "...";
-            }else{
+            } else {
                 return string;
             }
-
         }
-
-
+        
         function showCards(array) {
             cardWrapper.innerHTML = "";
             array.forEach((annuncio, i) => {
-                let div = document.createElement("div"); 
+                let div = document.createElement("div");
                 div.classList.add("card-custom");
                 div.innerHTML = `
                     <img src="https://picsum.photos/${300 + i}/200" alt="immagine casuale" class="img-fluid img-card">
@@ -53,71 +55,71 @@ fetch("./annunci.json").then((response)=> response.json()).then((data)=>{
         }
         
         showCards(data);
-
-
-        function filterByCategory(categoria){
-            if(categoria != "All"){   
-            let filtered = data.filter((annuncio)=> annuncio.category == categoria );
-            showCards(filtered);
-            }else{
-              showCards(data);  
-            }
-
-        }
-
+        
         let radioButtons = document.querySelectorAll(".form-check-input");
         
-        radioButtons.forEach((button)=>{
-            button.addEventListener("click", ()=>{
-                filterByCategory(button.id)
-            })
-        })
-
-
+        function filterByCategory(array) {
+            let checked = Array.from(radioButtons).find((button) => button.checked);
+            if (!checked) return array;
+            
+            let categoria = checked.id;
+            if (categoria != "All") {
+                let filtered = array.filter((annuncio) => annuncio.category == categoria);
+                return filtered;
+            } else {
+                return array;
+            }
+        }
+        
+        radioButtons.forEach((button) => {
+            button.addEventListener("click", () => {
+                setPriceInput();
+                globalFilter();
+            });
+        });
+        
         let priceInput = document.querySelector("#priceInput");
         let priceValue = document.querySelector("#priceValue");
-
-
-        function setPriceInput(){
-            let prices = data.map((annuncio)=> +annuncio.price);
-            prices.sort((a,b)=> a - b);
+        
+        function setPriceInput() {
+            let prices = filterByCategory(data).map((annuncio) => Number(annuncio.price));
+            prices.sort((a, b) => a - b);
             let maxPrice = Math.ceil(prices.pop());
             priceInput.max = maxPrice;
             priceInput.value = maxPrice;
             priceValue.innerHTML = maxPrice;
         }
-
-
+        
         setPriceInput();
-
-
-        function filterByPrice(){
-            let filtered = data.filter((annuncio)=> Number(annuncio.price) <= priceInput.value);
-            showCards(filtered);
+        
+        function filterByPrice(array) {
+            let filtered = array.filter((annuncio) => Number(annuncio.price) <= priceInput.value);
+            return filtered;
         }
-
-        priceInput.addEventListener("input", ()=>{
+        
+        priceInput.addEventListener("input", () => {
             priceValue.innerHTML = priceInput.value;
-            filterByPrice();
+            globalFilter();
         });
         
-        
-
         let wordInput = document.querySelector("#wordInput");
-
-        function filterbyWord(parola){
-            let filtered = data.filter ( (annuncio)=> annuncio.name.toLowerCase().includes(parola.toLowerCase()));
-            showCards(filtered);
-
+        
+        function filterbyWord(array) {
+            let filtered = array.filter((annuncio) => annuncio.name.toLowerCase().includes(wordInput.value.toLowerCase()));
+            return filtered;
         }
-
-        wordInput.addEventListener("input", ()=>{
-            filterbyWord(wordInput.value);
-        })
-
-
+        
+        wordInput.addEventListener("input", () => {
+            globalFilter();
+        });
+        
+        function globalFilter() {
+            let filteredByCategory = filterByCategory(data);
+            let filteredByPrice = filterByPrice(filteredByCategory);
+            let filteredByWord = filterbyWord(filteredByPrice);
+            showCards(filteredByWord);
+        }
     });
-
 
 
     
